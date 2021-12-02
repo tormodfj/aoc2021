1b7507d39fea8bbc28fcc23cec169e0651aa25e5
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2021-12-02 09:04:53 +0000

    Day 2

---
+++ 02-1/Program.fs
@@ -0,0 +1,27 @@
+open System
+open System.IO
+
+type Command =
+    | Forward of int
+    | Up of int
+    | Down of int
+
+let parseCommand (input:string) =
+    let command = input.Split(' ') |> List.ofArray
+    match command with
+    | "forward"::d::[] -> Forward(int d)
+    | "up"::d::[] -> Up(int d)
+    | "down"::d::[] -> Down(int d)
+    | _ -> failwith "unrecognized command"
+
+let executeCommand (horiz, depth) command =
+    match command with
+    | Forward(d) -> (horiz + d, depth)
+    | Up(d) -> (horiz, depth - d)
+    | Down(d) -> (horiz, depth + d)
+
+File.ReadAllLines("input")
+|> Seq.map parseCommand
+|> Seq.fold executeCommand (0, 0)
+|> fun(horiz, depth) -> horiz * depth 
+|> printf "%i"
---
+++ 02-2/Program.fs
@@ -0,0 +1,27 @@
+open System
+open System.IO
+
+type Command =
+    | Forward of int
+    | Up of int
+    | Down of int
+
+let parseCommand (input:string) =
+    let command = input.Split(' ') |> List.ofArray
+    match command with
+    | "forward"::d::[] -> Forward(int d)
+    | "up"::d::[] -> Up(int d)
+    | "down"::d::[] -> Down(int d)
+    | _ -> failwith "unrecognized command"
+
+let executeCommand (horiz, depth, aim) command =
+    match command with
+    | Forward(d) -> (horiz + d, depth + d*aim, aim)
+    | Up(d) -> (horiz, depth, aim - d)
+    | Down(d) -> (horiz, depth, aim + d)
+
+File.ReadAllLines("input")
+|> Seq.map parseCommand
+|> Seq.fold executeCommand (0, 0, 0)
+|> fun(horiz, depth, _) -> horiz * depth 
+|> printf "%i"
