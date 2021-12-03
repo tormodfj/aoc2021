361746d390e5fb46ded4b242eddf96796f6fe30f
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2021-12-03 09:51:59 +0100

    Day 3 - Part 1

---
+++ 03-1/Program.fs
@@ -0,0 +1,23 @@
+open System
+open System.IO
+
+let report =
+    File.ReadLines "input"
+    |> Seq.map List.ofSeq
+    |> List.transpose
+
+let binaryGammaRate =
+    report
+    |> List.map (List.countBy id)
+    |> List.map (List.maxBy (fun (_,n) -> n))
+    |> List.map (fun (c,_) -> c)
+
+let binaryEpsilonRate =
+    binaryGammaRate
+    |> List.map (fun c -> match c with |'0' -> '1'|'1' -> '0'|_ -> failwith "wtf")
+
+let binaryToInt cs = Convert.ToInt32(cs |> Array.ofList |> String, 2)
+let gammaRate = binaryGammaRate |> binaryToInt
+let epsilonRate = binaryEpsilonRate |> binaryToInt
+
+printf "%i" (gammaRate*epsilonRate)
