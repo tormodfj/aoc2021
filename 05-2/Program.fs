cafaa9c903d3ff50e5da1f257013748e67db85e8
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2021-12-06 13:33:34 +0100

    Day 5

---
+++ 05-2/Program.fs
@@ -0,0 +1,34 @@
+open System.IO
+open System.Text.RegularExpressions
+
+type Point = (int*int)
+type Line = { Start:Point; End:Point }
+
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+let parseLine input =
+    match input with
+    | Regex "(\d+),(\d+) -> (\d+),(\d+)" [x1;y1;x2;y2] -> { Start = (int x1, int y1); End = (int x2, int y2) }
+    | _ -> failwith "unrecognized input"
+
+let pointsFromLine { Start=(x1,y1); End=(x2,y2) } =  
+    let (..) a b = if a < b then seq { a..b } else seq { a..(-1)..b }
+
+    if x1=x2 then
+        seq { for y in y1..y2 -> (x1,y) }
+    else if y1=y2 then
+        seq { for x in x1..x2 -> (x,y1) }
+    else
+        Seq.zip { x1..x2 } { y1..y2 }
+
+let lines =
+    File.ReadLines "input"
+    |> Seq.map parseLine
+    |> Seq.collect pointsFromLine
+    |> Seq.countBy id
+    |> Seq.filter (fun (_,n) -> n >= 2)
+    |> Seq.length
+    |> printfn "%i"
