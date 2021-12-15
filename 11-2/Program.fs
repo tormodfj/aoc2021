a199e287b977d74d9e9f0c870de0cb62c969104e
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2021-12-15 19:59:09 +0100

    Day 11

---
+++ 11-2/Program.fs
@@ -0,0 +1,60 @@
+open System.IO
+
+type Octo = { Energy:int; Flashed:bool; FlashCount:int; Neighbours:int list }
+
+let charToInt (c:char) = (int c) - (int '0')
+let charToOcto (c:char) = { Energy = charToInt c; Flashed = false; FlashCount = 0; Neighbours = [] }
+let mapNeighbours (i:int) octo =
+    let neighbours = 
+        seq {
+            yield! seq { i-10; i+10 }
+            if (i % 10 <> 0) then yield! seq { i-11; i-1; i+9 }
+            if (i % 10 <> 9) then yield! seq { i-9; i+1; i+11 }
+        }
+        |> Seq.filter (fun i -> i >= 0 && i < 100)
+        |> List.ofSeq
+    { octo with Neighbours = neighbours }
+
+let octos =
+    File.ReadLines "input"
+    |> Seq.collect (fun line -> line |> Seq.map charToOcto)
+    |> Array.ofSeq
+    |> Array.mapi mapNeighbours
+
+let rec increaseEnergy i =
+    let before = octos[i]
+    let after = { before with Energy = before.Energy + 1 }
+    octos[i] <- after
+
+    match after with
+    | { Energy = 10; Flashed = false} -> flash i
+    | _ -> ()
+    
+and flash i =
+    let before = octos[i]
+    let after = { before with Flashed = true }
+    octos[i] <- after
+    
+    match before with
+    | { Flashed = false } -> before.Neighbours |> List.iter increaseEnergy
+    | _ -> ()
+
+let countAndReset i =
+    let before = octos[i]
+    let after =
+        match before with
+        | { Flashed = true} -> { before with Energy = 0; Flashed = false; FlashCount = before.FlashCount + 1 }
+        | _ -> before
+    octos[i] <- after
+
+let step () =
+    [0..99] |> List.iter increaseEnergy
+    [0..99] |> List.iter countAndReset
+
+let rec stepUntilAllFlashing n =
+    step()
+    let allFlashing = octos |> Array.forall (fun o -> o.Energy = 0)
+    if allFlashing then n else stepUntilAllFlashing (n+1)
+
+stepUntilAllFlashing 1
+|> printfn "%i"
